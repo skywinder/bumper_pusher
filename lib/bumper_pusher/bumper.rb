e94357cb9383bbe6b7e67e7bac6d0e710f7e8e5a
Petr Korolev <sky4winder@gmail.com>
2014-11-18 11:21:19 +0200

    put back version, remove dangerous lines

--- lib/bumper_pusher/bumper.rb
+++ lib/bumper_pusher/bumper.rb
@@ -228,8 +228,9 @@ module BumperPusher
           execute_line_if_not_dry_run("gem build #{@spec_file}")
           gem = find_current_gem_file
           execute_line_if_not_dry_run("gem install #{gem}")
-          execute_line_if_not_dry_run("git checkout #{version_file}")
-          execute_line_if_not_dry_run('git checkout README.md')
+
+          execute_line_if_not_dry_run("sed -i \"\" \"s/#{bumped_version}/#{result}/\" README.md")
+          execute_line_if_not_dry_run("sed -i \"\" \"s/#{bumped_version}/#{result}/\" #{version_file}")
         else
           raise 'Unknown spec type'
         end
